999fb3612820b03aac2ed5b2da085daa7fcaf5bf
abigailrubye <abigail.rubye2004@gmail.com>
2026-02-16 13:21:57 -0500

    sidebar delete

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Sidebar 1</p>
-        <Sidebar initialMenuItems={[
-            'Albert Eggstein',
-            'Attila the Hen',
-            'Dixie Chick',
-            'Gregory Peck',
-            'Mary Poopins'
-          ]}></Sidebar>
+        
       </header>
     </div>
   );
